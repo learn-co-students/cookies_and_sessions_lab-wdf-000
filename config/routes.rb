102b98bf269f44a6f51cc0a50d5a90d286cc45d2
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-08-19 21:35:07 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/', to: 'application#index' , as: 'index'
+  post '/', to: 'application#new' , as: 'new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
