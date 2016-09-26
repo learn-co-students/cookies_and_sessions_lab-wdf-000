5864321e1f2683118b257d7b42c3a94935c23ca0
ozPop <ozziepopovas@gmail.com>
2016-09-26 14:02:45 -0400

    add code to pass tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  get '/', to: 'products#index'
+  post '/', to: 'products#add'
+  get 'products/index', to: 'products#index'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
