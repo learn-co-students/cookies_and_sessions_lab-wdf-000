4b2c0c1858108742a1da0966e9c4bcf19e7554bd
Naveed Shah <naveed@tayybah.com>
2016-10-13 17:10:17 -0400

    Done.

---
+++ app/controllers/product_controller.rb
@@ -0,0 +1,10 @@
+class ProductController < ApplicationController
+  def index
+    @cart = session[:cart]
+  end
+
+  def add
+    session[:cart] = params[:product]
+      redirect_to '/'
+  end
+end
